310c2f7465359ff78fe6af7016001476e5d9dfa0
Mark Evers <mevers303@gmail.com>
2018-05-20 02:02:21 -0600

    Install script.

---
+++ install.sh
@@ -0,0 +1,10 @@
+mkdir data
+mkdir output
+
+mkdir data/fbo_files
+mkdir data/fbo_files/docs
+mkdir data/fbo_files/pickles
+
+mkdir data/govwin_opportunity
+mkdir data/govwin_opportunity/docs
+mkdir data/govwin_opportunity/pickles
