9c3f750fc57b08b292b7796a76e0282acf3e6b5b
Mark Evers <mevers303@gmail.com>
2018-05-22 02:26:57 -0600

    yeah

--- src/topics.py
+++ src/topics.py
@@ -5,10 +5,9 @@
 import numpy as np
 
 from nlp import nmf_model, get_corpus_top_topics
-from globals import *
-from vectorizer import count_vectorize_cache, cv_to_tfidf, count_vectorize, get_cached_corpus
-from data import get_db_corpus, check_corpus_pickles
-from sklearn.metrics.pairwise import pairwise_distances as pdist
+import globals as g
+from vectorizer import get_cached_corpus
+from sklearn.metrics.pairwise import pairwise_distances
 import matplotlib.pyplot as plt
 
 
@@ -16,15 +15,14 @@ import matplotlib.pyplot as plt
 
 def search_models(tfidf_corpus, min_topics, max_topics):
 
-    debug("Building NMF topics...")
+    g.debug("Building NMF topics...")
     nmf_models = []
-    reconstruction_errors = []
     costs = []
     intertopic_similarities = []
     interdocument_similarities = []
     n_models = max_topics - min_topics + 1
 
-    progress_bar(0, n_models)
+    g.progress_bar(0, n_models)
     for i in range(min_topics, max_topics + 1):
 
         nmf, W, H = nmf_model(tfidf_corpus, i, "", False, no_output=True)
@@ -32,21 +30,11 @@ def search_models(tfidf_corpus, min_topics, max_topics):
 
         nmf_models.append(nmf)
         costs.append(nmf.reconstruction_err_**2)
-        intertopic_similarities.append(1 - pdist(H, metric="cosine", n_jobs=-1).mean())
-        interdocument_similarities.append(np.mean([1 - pdist(tfidf_corpus[top_topics == topic_i].A, metric="cosine", n_jobs=-1).mean() for topic_i in range(i)]))
+        intertopic_similarities.append(1 - pairwise_distances(H, metric="cosine", n_jobs=-1).mean())
+        interdocument_similarities.append(np.mean([1 - pairwise_distances(tfidf_corpus[top_topics == topic_i].A, metric="cosine", n_jobs=-1).mean() for topic_i in range(i)]))
 
-        progress_bar(i - min_topics + 1, n_models, text=f"{nmf.n_iter_} iterations")
+        g.progress_bar(i - min_topics + 1, n_models, text=f"{nmf.n_iter_} iterations")
 
-    return nmf_models, costs, intertopic_similarities, interdocument_similarities
-
-
-def main():
-
-    min_topics = 5
-    max_topics = 10
-
-    doc_ids, tfidf_corpus = get_cached_corpus(TABLE_NAME, "tfidf")
-    nmf_models, costs, intertopic_similarities, interdocument_similarities = search_models(tfidf_corpus, min_topics, max_topics)
 
     fig, axes = plt.subplots(4, 1)
     axes = axes.flatten()
@@ -71,6 +59,17 @@ def main():
     plt.show()
     print("yeah")
 
+
+    return nmf_models, costs, intertopic_similarities, interdocument_similarities
+
+
+def main():
+
+    min_topics = 5
+    max_topics = 100
+
+    doc_ids, tfidf_corpus = get_cached_corpus(g.TABLE_NAME, "tfidf")
+    nmf_models, costs, intertopic_similarities, interdocument_similarities = search_models(tfidf_corpus, min_topics, max_topics)
 
 
 
