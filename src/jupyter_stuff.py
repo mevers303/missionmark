be2ebbb9f6bcc746a86ae662f7879bd87558f3ed
Mark Evers <mevers303@gmail.com>
2018-06-06 00:55:03 -0600

    Notebook stuff.

--- src/jupyter_stuff.py
+++ src/jupyter_stuff.py
@@ -14,18 +14,37 @@ def top7(topic_i, _W, _corpus, _doc_ids, _tfidf, _nmf, show_wordclouds=True, n_s
         display(Image(filename=f"../static/wordclouds/{topic_i}_nmf_wordcloud.png"))
         display(Image(filename=f"../static/wordclouds/{topic_i}_tfidf_wordcloud.png"))
 
-    print("TOP 7 MOST RELEVANT DOCUMENTS (auto-summarized)\n")
+    print(f"TOP 7 MOST RELEVANT DOCUMENTS FOR TOPIC {topic_i} (auto-summarized to {n_sentences} sentences)")
+    print(f"These are the {n_sentences} from each document most relevant to topic {topic_i}.\n")
 
     i = 0
     for doc, strength in get_top_10_docs(_W, topic_i):
         if i == 7:
             break
         print("*************************************************************")
-        print(_doc_ids[doc], " -> STRENGTH: ", round(strength * 100, 2), "%", sep="")
+        print("ID: ", _doc_ids[doc], " -> STRENGTH: ", round(strength * 100, 2), "%", sep="")
         print(summarize_doc_nmf(_corpus[doc], _tfidf, _nmf, topic_i, n_sentences=n_sentences))
         #print(summarize_doc(doc))
         print()
         i += 1
+
+
+def top7_to_file(topic_i, _W, _corpus, _doc_ids, _tfidf, _nmf, n_sentences=2, path="../static/wordclouds/"):
+
+    with open(path + str(topic_i) + "_summaries.txt", "w") as f:
+
+        f.write(f"TOP 7 MOST RELEVANT DOCUMENTS FOR TOPIC {topic_i} (auto-summarized to {n_sentences} sentences)\n")
+        f.write(f"These are the {n_sentences} from each document most relevant to topic {topic_i}.\n\n")
+
+        i = 0
+        for doc, strength in get_top_10_docs(_W, topic_i):
+            if i == 7:
+                break
+            f.write("*************************************************************\n")
+            f.write(f"ID: {_doc_ids[doc]} -> STRENGTH: {round(strength * 100, 2)}%\n\n")
+            f.write(summarize_doc_nmf(_corpus[doc], _tfidf, _nmf, topic_i, n_sentences=n_sentences))
+            f.write("\n\n")
+            i += 1
 
 
 def term_search(term, _H, _tfidf):
